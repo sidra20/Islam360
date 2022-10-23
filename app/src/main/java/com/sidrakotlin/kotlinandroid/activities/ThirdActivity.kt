939a8b95762064b6758f97d8ca2c26de63c2c75f
Sidra Paracha <sidraparacha27@gmail.com>
2022-10-23 00:45:53 -0700

    ignore binding

--- app/src/main/java/com/sidrakotlin/kotlinandroid/activities/ThirdActivity.kt
+++ app/src/main/java/com/sidrakotlin/kotlinandroid/activities/ThirdActivity.kt
@@ -1,4 +1,30 @@
 package com.sidrakotlin.kotlinandroid.activities
 
-class ThirdActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.sidrakotlin.kotlinandroid.R
+
+class ThirdActivity:AppCompatActivity() {
+    private lateinit var back:ImageView;
+    private lateinit var text:TextView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_third);
+
+        init();
+
+    }
+    private fun init()
+    {
+        back = findViewById(R.id.back)
+        text = findViewById(R.id.thirdLayoutText)
+
+        back.setOnClickListener {
+            finish()
+        }
+        text.text="Hello World!"
+    }
 }
