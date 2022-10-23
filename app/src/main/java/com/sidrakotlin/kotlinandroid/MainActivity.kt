ec0a727a489b8de880db084974bc021ab704d08f
Sidra Paracha <sidraparacha27@gmail.com>
2022-10-22 23:51:07 -0700

    view binding

--- app/src/main/java/com/sidrakotlin/kotlinandroid/MainActivity.kt
+++ app/src/main/java/com/sidrakotlin/kotlinandroid/MainActivity.kt
@@ -2,10 +2,29 @@ package com.sidrakotlin.kotlinandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sidrakotlin.kotlinandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+
+        binding.submit.setOnClickListener {
+            var name:String = binding.nameEt.text.toString();
+            if(name.isEmpty())
+            {
+                Toast.makeText(this, "Field is required!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Hello " + name, Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+
     }
 }
